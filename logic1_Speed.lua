2a2b34651ae26ced7fe100b407091fa3dc909ec3
Myth197 <59457254+Myth197@users.noreply.github.com>
2023-02-20 11:40:44 +0000

    Descriptive Names
    -Split Item names from location names, eg: Heli-pack now has the location name "Al's shop" and the item name "Heli-pack"
    -Change logic for Qwarks Trailer on Rilgar to require Heli or Thruster pack.
    -Change logic for Orxon vendor to require O2 mask
    -Make Gemlik require guns to shoot the towers
    -Allow a condition for using the sonic summoner on hoven to kill the helicopter

--- logic1_Speed.lua
+++ logic1_Speed.lua
@@ -1,80 +1,80 @@
 items = {
-    {id=0x02, name="Heli-pack",       req_items={} },
-    {id=0x03, name="Thruster-pack",   req_items={} },
-    {id=0x04, name="Hydro-pack",      req_items={{0x16}, {0x02}, {0x03}, {0x20}} },
-    {id=0x05, name="Sonic Summoner",  req_items={{0x30}} },
-    {id=0x06, name="O2 Mask",         req_items={{0x07}} },
-    {id=0x07, name="Pilot's Helmet",  req_items={} },
-    {id=0x09, name="Suck Cannon",     req_items={{0x02}, {0x03}, {0x20}} },
-    {id=0x0B, name="Devastator",      req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x0C, name="Swingshot",       req_items={} },
-    {id=0x0D, name="Visibomb",        req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x0E, name="Taunter",         req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x0F, name="Blaster",         req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x10, name="Pyrocitor",       req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x11, name="Mine Glove",      req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x12, name="Walloper",        req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x13, name="Tesla Claw",      req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x14, name="Glove of Doom",   req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x15, name="Morph-O-Ray",     req_items={{0x0C}, {0x20}} },
-    {id=0x16, name="Hydrodisplacer",  req_items={{0x1A}, {0x02}, {0x03}, {0x20}} },
-    {id=0x17, name="R.Y.N.O.",        req_items={{0x1B,0x02}, {0x1B,0x03}, {0x1B,0x20}} },
-    {id=0x18, name="Drone Device",    req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x19, name="Decoy Glove",     req_items={}, ill_items={0x30,0x31,0x32,0x34,0x35} },
-    {id=0x1A, name="Trespasser",      req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },
-    {id=0x1B, name="MetalDetector",   req_items={{0x1C}, {0x02}, {0x03}, {0x20}} },
-    {id=0x1C, name="Magneboots",      req_items={} },
-    {id=0x1D, name="Grindboots",      req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },
-    {id=0x1E, name="Hoverboard",      req_items={} },
-    {id=0x1F, name="Hologuise",       req_items={{0x1E,0x02}, {0x1E,0x0D}, {0x1E,0x19}, {0x03}, {0x20}} },
-    {id=0x20, name="PDA",             req_items={{0x1C}} },
-    {id=0x21, name="Map-O-Matic",     req_items={{0x1D}, {0x20}} },
-    {id=0x22, name="Bolt Grabber",    req_items={{0x04,0x06}, {0x20}, {0x02}, {0x03}} },
-    {id=0x23, name="Persuader",       req_items={{0x31,0x1A,0x16}} },
-    {id=0x30, name="Zoomerator",      req_items={{0x02}, {0x03}, {0x20}} },
-    {id=0x31, name="Raritanium",      req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },
-    {id=0x32, name="Codebot",         req_items={} },
-    {id=0x34, name="Premium Nanotech",req_items={{0x06,0x02}, {0x06,0x03}, {0x06,0x20}} },
-    {id=0x35, name="Ultra Nanotech",  req_items={{0x34,0x06,0x02}, {0x34,0x06,0x03}, {0x34,0x06,0x20}} },
+    {id=0x02, 	location="Al's Shop",			host_item="Heli-pack",			req_items={} },													 								--Kerwan
+    {id=0x03,	location="Bob's Shop",			host_item="Thruster-pack",		req_items={} },											 										--Poki
+    {id=0x04,	location="Edwina's Shop",		host_item="Hydro-pack",			req_items={{0x16}, {0x02}, {0x03}, {0x20}} },													--Hoven
+    {id=0x05,	location="Skidd's Agent",		host_item="Sonic Summoner",		req_items={{0x30}} },																			--Aridia
+    {id=0x06,	location="Ship Fight",			host_item="O2 Mask",			req_items={{0x07}} },																			--Poki
+    {id=0x07,	location="Pilot's Helmet",		host_item="Pilot's Helmet",		req_items={} },																					--Gaspar
+    {id=0x09,	location="Suck Cannon",			host_item="Suck Cannon",		req_items={{0x02}, {0x03}, {0x20}} },															--Eudora
+    {id=0x0B,	location="Vendor",				host_item="Devastator",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Batalia
+    {id=0x0C,	location="Fitness Course",		host_item="Swingshot",			req_items={} },																					--Kerwan
+    {id=0x0D,	location="Vendor",				host_item="Visibomb",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Orxon
+    {id=0x0E,	location="Vendor",				host_item="Taunter",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Blarg
+    {id=0x0F,	location="Vendor",				host_item="Blaster",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Kerwan
+    {id=0x10,	location="Vendor",				host_item="Pyrocitor",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Novalis
+    {id=0x11,	location="Vendor",				host_item="Mine Glove",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Rilgar
+    {id=0x12,	location="Vendor",				host_item="Walloper",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Gaspar
+    {id=0x13,	location="Vendor",				host_item="Tesla Claw",			req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Oltanis
+    {id=0x14,	location="Vendor",				host_item="Glove of Doom",		req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Eudora
+    {id=0x15,	location="Morph-O-Ray",			host_item="Morph-O-Ray",		req_items={{0x0C}, {0x20}} },																	--Oltanis
+    {id=0x16,	location="Outside",				host_item="Hydrodisplacer",		req_items={{0x1A}, {0x02}, {0x03}, {0x20}} },													--Blarg
+    {id=0x17,	location="Shady Salesman",		host_item="R.Y.N.O.",			req_items={{0x1B, 0x02}, {0x1B, 0x03}, {0x1B, 0x20}} },											--Rilgar
+    {id=0x18,	location="Vendor",				host_item="Drone Device",		req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Hoven
+    {id=0x19,	location="Vendor",				host_item="Decoy Glove",		req_items={},											ill_items={0x30,0x31,0x32,0x34,0x35} },	--Poki
+    {id=0x1A,	location="Construction Zone",	host_item="Trespasser",			req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },													--Aridia
+    {id=0x1B,	location="Turret Fight", 		host_item="MetalDetector",		req_items={{0x1C}, {0x02}, {0x03}, {0x20}} },													--Batalia
+    {id=0x1C,	location="Clank 1st Item",		host_item="Magneboots",			req_items={} },																					--Orxon
+    {id=0x1D,	location="Alien Queen",			host_item="Grindboots",			req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },													--Blarg
+    {id=0x1E,	location="Save Skidd",			host_item="Hoverboard",			req_items={} },																					--Aridia
+    {id=0x1F,	location="Hoverboard Race",		host_item="Hologuise",			req_items={{0x1E, 0x02}, {0x1E, 0x0D}, {0x1E, 0x19}, {0x03}, {0x20}} },							--KaleboIII
+    {id=0x20,	location="Steve",				host_item="PDA",				req_items={{0x1C}} },																			--Oltanis
+    {id=0x21,	location="Grind Rail",			host_item="Map-O-Matic",		req_items={{0x1D}, {0x20}} },																	--KaleboIII
+    {id=0x22,	location="Water path",			host_item="Bolt Grabber",		req_items={{0x04, 0x06}, {0x20}, {0x02}, {0x03}} },												--Quartu
+    {id=0x23,	location="Water Lab",			host_item="Persuader",			req_items={{0x31, 0x1A, 0x16}, {0x31,0x06}} },													--Poki
+    {id=0x30,	location="Hoverboard Race",		host_item="Zoomerator",			req_items={{0x02}, {0x03}, {0x20}} },															--Rilgar
+    {id=0x31,	location="Drill Guy",			host_item="Raritanium",			req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },													--Hoven
+    {id=0x32,	location="Water path",			host_item="Codebot",			req_items={} },																					--Fleet
+    {id=0x34,	location="Nanotech Vendor 1",	host_item="Premium Nanotech",	req_items={{0x06, 0x02}, {0x06, 0x03}, {0x06, 0x20}} },											--Orxon
+    {id=0x35,	location="Nanotech Vendor 2",	host_item="Ultra Nanotech",		req_items={{0x34, 0x06, 0x02}, {0x34, 0x06, 0x03}, {0x34, 0x06, 0x20}} },						--Orxon
 }
 
 infobots = {
-    {id=0x01, req_items={} },				                        -- Veldin1 -> Novalis
-    {id=0x02, req_items={} },				                        -- Novalis -> Aridia
-    {id=0x03, req_items={} },				                        -- Novalis -> Kerwan
-    {id=0x04, req_items={{0x02}, {0x03}, {0x20}} },         -- Kerwan -> Eudora
-    {id=0x05, req_items={} },				                        -- Blarg -> Rilgar
-    {id=0x06, req_items={{0x02}, {0x03}, {0x20}, {0x19}} }, -- Eudora -> Blarg
-    {id=0x07, req_items={{0x02}, {0x03}, {0x20}, {0x0C}} }, -- Rilgar -> Umbris
-    {id=0x08, req_items={{0x0C}, {0x02}, {0x03}, {0x20}} }, -- Umbris -> Batalia
-    {id=0x09, req_items={{0x1D}, {0x02}, {0x20}} },         -- Batalia -> Gaspar
-    {id=0x0A, req_items={} },				                        -- Batalia -> Orxon
-    {id=0x0B, req_items={} },				                        -- Orxon -> Pokitaru
-    {id=0x0C, req_items={{0x06,0x02}, {0x06,0x03}, {0x06,0x20}} },         -- Orxon -> Hoven
-    {id=0x0D, req_items={} },				                                       -- Hoven -> Gemlik
-    {id=0x0E, req_items={{0x0C,0x1C,0x1A,0x0B}, {0x02}, {0x03}, {0x20}} }, -- Gemlik -> Oltanis
-    {id=0x0F, req_items={{0x1D}, {0x1C}, {0x20}} },         -- Oltanis -> Quartu
-    {id=0x10, req_items={{0x0C}, {0x02}, {0x03}, {0x20}} }, -- Quartu -> KaleboIII
-    {id=0x11, req_items={{0x02}, {0x03}, {0x20}} },         -- Quartu -> Fleet
-    {id=0x12, req_items={{0x03}, {0x1C}, {0x19}, {0x20}} }  -- Fleet -> Veldin2
+    {id=0x01, req_items={} },												-- Novalis "infobot" on Veldin1 Clank
+    {id=0x02, req_items={} },												-- Aridia infobot on Novalis, Water works
+    {id=0x03, req_items={} },												-- Kerwan infobot on Novalis, Save the Chairman 
+    {id=0x04, req_items={{0x02}, {0x03}, {0x20}} },							-- Eudora infobot on Kerwan, Train
+    {id=0x05, req_items={} },												-- Rilgar infobot on Blarg, Warship
+    {id=0x06, req_items={{0x02}, {0x03}, {0x20}, {0x19}} },					-- Blarg infobot on Eudora, Robot Lieutenant
+    {id=0x07, req_items={{0x02}, {0x03}, {0x20}, {0x0C}} },					-- Umbris infobot on Rilgar, Qwark's Trailer
+    {id=0x08, req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },					-- Batalia infobot on Umbris, Snagglebeast
+    {id=0x09, req_items={{0x1D}, {0x02}, {0x20}} },							-- Gaspar infobot on Batalia, Grind Rail
+    {id=0x0A, req_items={} },												-- Orxon infobot on Batalia, Commando
+    {id=0x0B, req_items={} },												-- Pokitaru infobot on Orxon, Clank 2
+    {id=0x0C, req_items={{0x06,0x02}, {0x06,0x03}, {0x06,0x20}} },			-- Hoven infobot on Orxon, Chase the infobot
+    {id=0x0D, req_items={} },												-- Gemlik infobot on Hoven, Turret Fight
+    {id=0x0E, req_items={{0x0C,0x1C,0x1A,0x0B}, {0x02}, {0x03}, {0x20}} },	-- Oltanis infobot on Gemlik, Defeat Qwark
+    {id=0x0F, req_items={{0x1D}, {0x1C}, {0x20}} },							-- Quartu infobot on Oltanis, Deaf guy
+    {id=0x10, req_items={{0x0C}, {0x02}, {0x03}, {0x20}} },					-- KaleboIII infobot on Quartu, Giant Clank Fight
+    {id=0x11, req_items={{0x02}, {0x03}, {0x20}} },							-- Fleet infobot on Quartu, Clank's Mum
+    {id=0x12, req_items={{0x03}, {0x1C}, {0x19}, {0x20}} }					-- Veldin2 infobot on Drek's Fleet, Battle through the ships
 }
 
 planets = {
-    {id=1,  name="Novalis",  infobots={0x02,0x03},  items={0x10} },
-    {id=2,  name="Aridia",   infobots={},           items={0x1E,0x1A,0x05} },
-    {id=3,  name="Kerwan",   infobots={0x04},       items={0x0C,0x02,0x0F} },
-    {id=4,  name="Eudora",   infobots={0x06},       items={0x14,0x09} },
-    {id=5,  name="Rilgar",   infobots={0x07},       items={0x30,0x11,0x17} },
-    {id=6,  name="Blarg",    infobots={0x05},       items={0x1D,0x16,0x0E} },
-    {id=7,  name="Umbris",   infobots={0x08},       items={} },
-    {id=8,  name="Batalia",  infobots={0x09,0x0A},  items={0x1B,0x0B} },
-    {id=9,  name="Gaspar",   infobots={},           items={0x07,0x12} },
-    {id=10, name="Orxon",    infobots={0x0B,0x0C},  items={0x34,0x35,0x1C,0x0D} },
-    {id=11, name="Pokitaru", infobots={},           items={0x03,0x06,0x23,0x19} },
-    {id=12, name="Hoven",    infobots={0x0D},       items={0x31,0x04,0x18} },
-    {id=13, name="Gemlik",   infobots={0x0E},       items={} },
-    {id=14, name="Oltanis",  infobots={0x0F},       items={0x20,0x13,0x15} },
-    {id=15, name="Quartu",   infobots={0x10,0x11},  items={0x22} },
-    {id=16, name="KaleboIII",infobots={},           items={0x1F,0x21} },
-    {id=17, name="Fleet",    infobots={0x12},       items={0x32} }
+    {id=1,  location="Clank",				host_item="Novalis",  infobots={0x02,0x03},  items={0x10} },				--Veldin1
+    {id=2,  location="Water works",			host_item="Aridia",   infobots={},           items={0x1E,0x1A,0x05} },		--Novalis
+    {id=3,  location="Save the Chairman",	host_item="Kerwan",   infobots={0x04},       items={0x0C,0x02,0x0F} },		--Novalis
+    {id=4,  location="Train",				host_item="Eudora",   infobots={0x06},       items={0x14,0x09} },			--Kerwan
+    {id=5,  location="Warship",				host_item="Rilgar",   infobots={0x07},       items={0x30,0x11,0x17} },		--Blarg
+    {id=6,  location="Robot Lieutenant",	host_item="Blarg",    infobots={0x05},       items={0x1D,0x16,0x0E} },		--Eudora
+    {id=7,  location="Qwark's Trailer",		host_item="Umbris",   infobots={0x08},       items={} },					--Rilgar
+    {id=8,  location="Snagglebeast",		host_item="Batalia",  infobots={0x09,0x0A},  items={0x1B,0x0B} },			--Umbris
+    {id=9,	location="Grind Rail",			host_item="Gaspar",   infobots={},           items={0x07,0x12} },			--Batalia
+    {id=10, location="Commando",			host_item="Orxon",    infobots={0x0B,0x0C},  items={0x34,0x35,0x1C,0x0D} },	--Batalia
+    {id=11, location="Clank 2nd Item",		host_item="Pokitaru", infobots={},           items={0x03,0x06,0x23,0x19} },	--Orxon
+    {id=12, location="Chase the Infobot",	host_item="Hoven",    infobots={0x0D},       items={0x31,0x04,0x18} },		--Orxon
+    {id=13, location="Turret Fight",		host_item="Gemlik",   infobots={0x0E},       items={} },					--Hoven
+    {id=14, location="Defeat Qwark",		host_item="Oltanis",  infobots={0x0F},       items={0x20,0x13,0x15} },		--Gemlik
+    {id=15, location="Deaf Guy",			host_item="Quartu",   infobots={0x10,0x11},  items={0x22} },				--Oltanis
+    {id=16,	location="Giant Clank Fight",	host_item="KaleboIII",infobots={},           items={0x1F,0x21} },			--Quartu
+    {id=17,	location="Clank's Mum",			host_item="Fleet",    infobots={0x12},       items={0x32} }					--Quartu
 }
