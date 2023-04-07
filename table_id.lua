034eab14340a130e361f6b904732f62973352a1f
echo storrs <echoatday@gmail.com>
2023-04-07 19:30:47 -0400

    used table_id to make logic files more editable

---
+++ table_id.lua
@@ -0,0 +1,68 @@
+-- key item ids
+keyid = {
+-- packs
+	heli_pack=2,
+	thruster_pack=3,
+	hydro_pack=4,
+-- helmets
+	sonic_summoner=5,
+	o2_mask=6,
+	pilots_helmet=7,
+-- gadgets
+	swingshot=0xc,
+	hydrodisplacer=0x16,
+	trespasser=0x1a,
+	metal_detector=0x1b,
+    hologuise=0x1f,
+    pda=0x20,
+-- boots
+	magneboots=0x1c,
+	grindboots=0x1d,
+-- items
+	hoverboard=0x1e,
+	map_o_matic=0x21,
+	bolt_grabber=0x22,
+	persuader=0x23,
+	zoomerator=0x30,
+	raritanium=0x31,
+	codebot=0x32,
+	premium_nanotech=0x34,
+	ultra_nanotech=0x35,
+-- vendor weapons
+	devastator=0xb,
+	visibomb=0xd,
+	taunter=0xe,
+	blaster=0xf,
+	pyrociter=0x10,
+	mine_glove=0x11,
+	walloper=0x12,
+	tesla_claw=0x13,
+	glove_of_doom=0x14,
+	drone_device=0x18,
+	decoy_glove=0x19,
+-- check weapons
+	bomb_glove=10, -- currently unused
+	suck_cannon=0x9,
+	morph_o_ray=0x15,
+	ryno=0x17,
+	
+-- infobots
+	novalis=1,
+	aridia=2,
+	kerwan=3,
+	eudora=4,
+	rilgar=5,
+	blarg=6,
+	umbris=7,
+	batalia=8,
+	gaspar=9,
+	orxon=10,
+	pokitaru=0xb,
+	hoven=0xc,
+	gemlik=0xd,
+	oltanis=0xe,
+	quartu=0xf,
+	kalebo=0x10,
+	fleet=0x11,
+	veldin=0x12,
+}
