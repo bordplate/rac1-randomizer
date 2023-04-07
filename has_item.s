6339731b29331c105adbd1fe8cab7f9abcb76efd
bordplate <vetle@bordplate.no>
2023-04-07 18:06:46 +0200

    Changed error in comments in .s files that were using ';' instead of the correct '#'

--- has_item.s
+++ has_item.s
@@ -1,9 +1,9 @@
-; We store what items the player has gotten at 0xB00050
-; This has_item function checks that memory to see if the
-;	player has the item.
+# We store what items the player has gotten at 0xB00050
+# This has_item function checks that memory to see if the
+#	player has the item.
 
 
-; bool has_item(int item)
+# bool has_item(int item)
 entry:
 	addis      r3,r3,0xb0
 	lbz        r3,0x50(r3)
@@ -11,10 +11,10 @@ entry:
 	blr
 
 
-; bool has_item(int item) {
-; 	if (&(0xB00050 + item) == 1) {
-;		return true;
-;	}
-;	
-;	return false;
-; }
+# bool has_item(int item) {
+# 	if (&(0xB00050 + item) == 1) {
+#		return true;
+#	}
+#	
+#	return false;
+# }
